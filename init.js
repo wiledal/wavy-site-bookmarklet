b62c7dd1120d39ee844707d09da993c59747a35d
Hugo Wiledal <hugo.wiledal@gmail.com>
2020-05-22 14:14:05 +0200

    wavy site

--- init.js
+++
@@ -1,5 +0,0 @@
-javascript: var s = document.createElement("script");
-s.src =
-  "https://raw.githubusercontent.com/wiledal/wavy-site-bookmarklet/master/wavy.js";
-s.type = "text/javascript";
-document.body.appendChild(s);
